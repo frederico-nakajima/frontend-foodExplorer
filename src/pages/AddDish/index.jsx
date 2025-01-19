93cfdb569150eeae85da17b6b1aca8ce3eba0e65
frederico <fredericonakajimanakajima@gmail.com>
2025-01-19 05:38:10 -0300

    refatoração addDish

--- src/pages/AddDish/index.jsx
+++ src/pages/AddDish/index.jsx
@@ -7,7 +7,6 @@ import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import UploadSimple from '../../assets/UploadSimple.svg';
 import {Link} from 'react-router-dom'
-
 import { Footer } from '../../components/Footer'
 import CaretLeft from '../../assets/CaretLeft.svg';
 
@@ -20,76 +19,72 @@ export function AddDish(){
             <div className="header-desktop">
                 <DesktopHeader />
             </div>
+
             <Link to="/">
                 <img src={CaretLeft} alt="imagem de uma seta apontando para esquerda" />
                 Voltar
-            </Link>
-                        
+            </Link>                        
             
-            <main>
-                <Form>
-                    
-                    <header>
-                        <h1 className='adicionarPrato'>Adicionar prato</h1>
-                        <h1 className='novoPrato'>Novo prato</h1>
-                    </header>
-                        
-    
-                    <div className="imagem-salada-categoria">
-                        <div className='UploadSimple'>
-                            <label htmlFor="UploadSimple">Imagem do prato</label>
-                            <div>
-                                <img src={UploadSimple} alt="ícone de upload" />
-                                <input  placeholder="Selecione imagem"  id="UploadSimple"/>
-                            </div>
+            <Form>                    
+                <header>
+                    <h1 className='adicionarPrato'>Adicionar prato</h1>
+                    <h1 className='novoPrato'>Novo prato</h1>
+                </header>                        
+
+                <div className="imagem-salada-categoria">
+                    <div className='UploadSimple'>
+                        <label htmlFor="UploadSimple">Imagem do prato</label>
+                        <div>
+                            <img src={UploadSimple} alt="ícone de upload" />
+                            <input type='file' placeholder="Selecione imagem"  id="UploadSimple"/>
                         </div>
-    
-                        <div className="inputs">
-                            <div className='nome'>
-                                <label htmlFor="nome">Nome</label>
-                                <Input placeholder="Ex.: Salada Ceasar" id="nome"/>
-                            </div>
-                            <div className="dados">
-                                <label htmlFor="categoria">Categoria</label>
-                                <select id="categoria">
+                    </div>
+
+                    <div className="inputs">
+                        <div className='nome'>
+                            <label htmlFor="nome">Nome</label>
+                            <Input placeholder="Ex.: Salada Ceasar" id="nome"/>
+                        </div>
+                        <div className="dados">
+                            <label htmlFor="categoria">Categoria</label>
+                            <select id="categoria">
                                 <option value="live">Refeição</option>
                                 <option value="podcast">Sobremesa</option>
                                 <option value="mentorship">Bebida</option>
-                                </select>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section">
+                    <div className="ingredientes-preco">
+                        <label htmlFor="preco">Ingredientes</label>
+                        <Section>
+                            <div className='tags'>
+                                <NoteItem  placeholder="Pão Naan"/>
+                                <NoteItem isnew placeholder="Adicionar"/>
                             </div>
-                        </div>
+                        </Section>
                     </div>
-    
-                    <div className="section">
-                        <div className="ingredientes-preco">
-                            <label htmlFor="preco">Ingredientes</label>
-                            <Section>
-                                <div className='tags'>
-                                    <NoteItem  placeholder="Pão Naan"/>
-                                    <NoteItem isnew placeholder="Adicionar"/>
-                                </div>
-                            </Section>
-                        </div>
-                        <div className='preco'>
-                            <label htmlFor="preco">Preço</label>
-                            <Input placeholder="R$ 00,00" id="preco"/>
-                        </div>
+                    
+                    <div className='preco'>
+                        <label htmlFor="preco">Preço</label>
+                        <Input placeholder="R$ 00,00" id="preco"/>
                     </div>
-    
-                    <div className="descricao">
-                        <label htmlFor="descricao">Descrição</label>
-                        <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" id="descricao"/>
-                    </div>
+                </div>
 
-                    <div className="buttons">
-                        <button>Salvar alterações</button>
-                    </div>
+                <div className="descricao">
+                    <label htmlFor="descricao">Descrição</label>
+                    <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" id="descricao"/>
+                </div>
 
-                </Form>
-            </main>
+                <div className="buttons">
+                    <button>Salvar alterações</button>
+                </div>
+            </Form>      
+            
                         
-            <Footer/>
-           
+            <Footer/>           
         </Container>
     )
 }
--- src/pages/AddDish/styles.js
+++ src/pages/AddDish/styles.js
@@ -2,396 +2,243 @@ import styled from 'styled-components';
 import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.div`
+    .header-mobile {
+        display: flex;         
+    }
+    .header-desktop {
+        display: none;
+    }
+
     display: flex;
     flex-direction: column;
-    align-items: center;
-   
-    
+    align-items: center;    
     width:428px;
-    margin:0 auto;
-    .header-mobile {
-        display: flex; /* Exibe o BrandHeader no mobile */
-        
-    }
-
-    .header-desktop {
-        display: none; /* Oculta o Header no mobile */
-    }
-    
+    margin:0 auto;    
               
     >a{
-     width: 364px;
-     display:flex;
-     align-items:center;
-     color: ${({theme}) => theme.COLORS.WHITE};;
-     font-family: Poppins;
-     font-size: 16.546px;
-     font-style: normal;
-     font-weight: 500;
-     line-height: 140%; /* 23.164px */
-     padding-bottom:150px;
-     >svg{
-         width: 8px;
-         height: 15px;
-     }
-     
-    
-    }           
-
-    >main{
-        display: flex;
-        flex-direction: column;
-        width:428px;
-       padding-bottom:200px;
+        width: 364px;
+        display:flex;
         align-items:center;
-
-        .imagem-salada-categoria{
-                
-            width: 364px;
-            height: 300px;
-            display: flex;
-            flex-direction: column;
-            
-            
-            .UploadSimple{
-                display:flex;
-                flex-direction:column;
-                width:270px;
-             
-
-                >div{
-                  border:1px solid ${({theme}) => theme.COLORS.WHITE};
-                    display:flex;
-                    align-items:center;
-                    width: 364px;
-                    padding-left:25px;
-                   
-                    border-radius: 5px;
-                    background-color:${({theme}) => theme.COLORS.BACKGROUND_800};   
-                    >input{
-                        border-radius: 5px;
-                        height: 48px;
-                        border:0;
-                        margin-left:5px;
-                        background-color:${({theme}) => theme.COLORS.BACKGROUND_800};
-                        outline:none;
-                         
-                        &::placeholder {
-                            color:${({theme}) => theme.COLORS.GRAY_300};
-                            font-family: "Roboto", serif;
-                            font-size: 16px;
-                            font-style: normal;
-                            font-weight: 400;
-                            margin-left:5px;
-                            text-align:left;
-                            }
-                    }
-                        
-                        
-        }
-                
-    }
-    
-              
-    .inputs{
-        
-        width: 364px;
-        height: 541px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        gap:32px;
-        
-        
-        .nome{
-           width: 364px;
-            
-        }
-        
-        .dados{
-            display: flex;
-            flex-direction:column;
-            align-items: flex-start;
-            
-            >label{
-                margin-top:-8px;
-                
-        }
-        
-    }
-        
-    select{
-        border-radius: 4px;
-        width: 364px;
-        height: 48px;
-        background-color:${({theme}) => theme.COLORS.BACKGROUND_800};
-        appearance: none;
-        -webkit-appearance: none;
-
-        background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_1_54)'%3E%3Cpath d='M8 10L12 14L16 10' stroke='%239C98A6' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_1_54'%3E%3Crect width='24' height='24' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A");
-
-        background-repeat:no-repeat;
-        background-position: right 24px top 50%;
-        color: #C1BCCC;
+        color: ${({theme}) => theme.COLORS.WHITE};;
         font-family: Poppins;
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 26px; /* 162.5% */
-        padding:0 24px;
-        border:0;
-        margin-top:4px; 
-    }
-        
-}
-}
-                                    
-                                
-    .section{
-        width: 364px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-bottom:15px;
-    
-        
-        .preco{
-            display: flex;
-            flex-direction:column;
-            align-items: flex-start;
-        >label{
-            margin-bottom:5px;
-           
-        }
-        }
-        
-    }
-        
-        
-
-
-    label{
-        color:${({theme}) => theme.COLORS.GRAY_200};
-        font-family: Roboto;
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 100%; /* 16px */
-        margin-bottom:8px;
-    }
-
-    
-
-
-    .buttons{
-    display: flex;
-    justify-content: flex-end;
-    align-items:center;
-    margin-top:13px;
-
-    >button{
-    width: 364px;
-        height: 48px;
-        padding-right:15px;
-        padding-left:15px;
-        border-radius: 5px;
-        color:${({theme}) => theme.COLORS.WHITE} ;
-        background-color: ${({ theme }) => theme.COLORS.RED_200};
-        border:0;
-        text-align: center;
-        font-family: Poppins;
-        font-size: 14px;
-        font-style: normal;
+        font-size: 16.5px;
         font-weight: 500;
-        line-height: 24px; /* 171.429% */
+        line-height: 140%;
+        padding-bottom:30px;
+        >svg{
+            width: 8px;
+            height: 15px;
+        }   
     
+    }  
+    
+                     
+    @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {            
+        .header-mobile {
+            display: none; 
+        }
+        .header-desktop {
+            display: block; 
+        } 
+
+        width:100%; 
         
-
-    }
-
-    }
-
-
-
-    .tags{
-    display: flex;
-    align-items: center;
-    height: 51px;
-    gap: 15px;
-    width: 265px;
-    background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
-    padding: 0px 0px 0px 0px;
-    border-radius: 8px;
-
-    margin-bottom:20px;
-
-    }
-
-
-    }
-    
-                    
-@media (min-width: ${DEVICE_BREAKPOINTS.LG}) {            
-    width:1918px; 
-       
-  .header-mobile {
-      display: none; /* Oculta o BrandHeader no desktop */
-    }
-
-    .header-desktop {
-      display: block; /* Exibe o Header no desktop */
-    }   
-    >a{
-        margin-top:40px;
-        width:1150px;
-        padding-bottom:24px;
-    >svg{
-    }
-    }
-    >main{
-        height:640px;
-    }
+        >a{
+            margin-top:40px;
+            width:1150px;
+            padding-bottom:24px;     
+        }
+      
     }
 `;    
         
        
     
 export const Form = styled.form`
-    width: 364px;
-    height: 541px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+     
+  
+   
+    padding-bottom:100px;
        
     
     >header{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-
-        h1.adicionarPrato{
+        .adicionarPrato{
             display:none;
-            font-family: Poppins;
-            font-size: 32px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 140%; 
-            
         }
-         h1.novoPrato{
+         .novoPrato{
             display:block;
             font-family: Poppins;
             font-size: 32px;
-            font-style: normal;
             font-weight: 500;
             line-height: 140%; 
             margin-bottom:24px;
+            width: 364px;
+        }
+    }
+            
+    .imagem-salada-categoria{                
+        width: 364px;
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        
+        
+        .UploadSimple{
+            display:flex;
+            flex-direction:column;
+           
+        
+
+            >div{
+                border:1px solid ${({theme}) => theme.COLORS.WHITE};
+                display:flex;
+                align-items:center;
+                width: 364px;
+                padding-left:25px;
+            
+                border-radius: 5px;
+                background-color:${({theme}) => theme.COLORS.BACKGROUND_800};   
+                >input{
+                    border-radius: 5px;
+                    height: 48px;
+                    border:0;
+                    margin-left:5px;
+                    background-color:${({theme}) => theme.COLORS.BACKGROUND_800};
+                    outline:none;
+                    
+                    &::placeholder {
+                        color:${({theme}) => theme.COLORS.GRAY_300};
+                        font-family: "Roboto", serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        margin-left:5px;
+                        text-align:left;
+                    }
+                }
+            }
+        }
+
+        
+        .inputs{            
+            width: 364px;
+            height: 541px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            gap:32px;             
+            
+            .nome{
+                width: 364px;
+                
+            }
+            
+            .dados{
+                display: flex;
+                flex-direction:column;
+                align-items: flex-start;
+                
+                >label{
+                    margin-top:-8px;
+                    
+                }
+            
+            }
+    
+            select{
+                border-radius: 4px;
+                width: 364px;
+                height: 48px;
+                background-color:${({theme}) => theme.COLORS.BACKGROUND_800};
+                appearance: none;
+                -webkit-appearance: none;
+                background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_1_54)'%3E%3Cpath d='M8 10L12 14L16 10' stroke='%239C98A6' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_1_54'%3E%3Crect width='24' height='24' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A");
+
+                background-repeat:no-repeat;
+                background-position: right 24px top 50%;
+                color: #C1BCCC;
+                font-family: Poppins;
+                font-size: 16px;
+                font-weight: 400;
+                line-height: 26px; 
+                padding:0 24px;
+                border:0;
+                margin-top:4px; 
+            }
+    
+        }
+
+        .section{
+            width: 364px;
+            
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-bottom:15px;
+              
+       
+        
+        
+        
+            >.ingredientes-preco{
+                display: flex;
+                flex-direction:row;
+                justify-content: center;
+                
+                >label{
+                    margin-bottom:5px;
+                
+                }
+            }
             
         }
-    }
-    
-
-
-    @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
-    
+            
    
-    
-    width: 1137px;
-    header{
-    h1.novoPrato{
-        display:none;
-    }
 
-    h1.adicionarPrato{
-            width: 1137px;
-            display:block;
-    }
-    }
-    div.imagem-salada-categoria{
-        width: 1137px;
-        display: flex;
-        flex-direction: row;
-        align-items:center;
-        
-        gap: 34px;
-
-    >div.UploadSimple{
-        width:200px;
-
-        div{
-        width: 224px;
-        
-        }
-    }
-
-    >div.inputs{
-        
-        display: flex;
-        flex-direction: row;
-        align-items:center;
-        height:64px;
-        width:1010px;
-        gap:10px;
         
 
-        div.nome{
-            width:530px;
-            height:67px;
-        
-        }
 
-        div.dados{
+        .buttons{
+            display: flex;
             justify-content: flex-end;
-            height:75px;
-            select{
-                border:1px solid ${({ theme }) => theme.COLORS.WHITE};
+            align-items:center;
+            margin-top:13px;
+
+            >button{
+                width: 364px;
+                height: 48px;
+                padding-right:15px;
+                padding-left:15px;
+                border-radius: 5px;
+                color:${({theme}) => theme.COLORS.WHITE} ;
+                background-color: ${({ theme }) => theme.COLORS.RED_200};
+                border:0;
+                text-align: center;
+                font-family: Poppins;
+                font-size: 14px;
+                font-style: normal;
+                font-weight: 500;
+                line-height: 24px; /* 171.429% */
             }
-                
+
         }
-    }
-    } 
-
-
-
     
-    div.section{
-    width: 1137px;
-    display: flex;
-    flex-direction: row;
-    justify-content:space-between;
+    }
 
-
-    div.ingredientes-preco{
-    display: flex;
-    flex-direction: column;
-    >Section{
-        div.tags{
-            border:1px solid ${({ theme }) => theme.COLORS.WHITE};
-            width: 909px;
-
-            >NoteItem{
-                width: 265px;
+    @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {    
+        header{
+            .novoPrato{
+                display:none;
+            }                
+            .adicionarPrato{
+                width: 1137px;
+                display:block;
             }
         }
-    }
-    }
-    div.preco{
-        height:77px;
-        justify-content:center;
-        
+                    
+        width: 1137px;
     }
 
-    }   
-    
-    
-    div.buttons{
-        button{
-            width: 165px;
-       
-        }
-    
-    }
-}
+     
 `;
         
         
