b4fe67bc3a050bc374b3243ed8eeb732d6b9b875
frederico <fredericonakajimanakajima@gmail.com>
2025-03-26 14:36:10 -0300

    height:100vh; no dishmobile

--- src/pages/DishCommonUser/styles.js
+++ src/pages/DishCommonUser/styles.js
@@ -7,7 +7,8 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;    
-    width: 100%;     
+    width: 100%;
+    height:100vh;      
     .mobile-header {
         display: flex;
         margin-bottom:10rem;
